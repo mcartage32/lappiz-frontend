cc2337659d75e1caefa19003ff65289434f82463
mcartage32 <mcartage33@gmail.com>
2026-04-22 00:55:51 -0500

    Ajuste iniciales en el eslint y en el viteconfig. Se ajustan etiquetas necesarias en el main y se agrega el provider para las notificaciones de ant design

---
+++ src/componets/NotificationCustom.tsx
@@ -0,0 +1,54 @@
+import { useEffect, type ReactNode } from "react";
+import { notification, type NotificationArgsProps } from "antd";
+import type { NotificationInstance } from "antd/es/notification/interface";
+
+// Referencia global al API de notificaciones
+let notificationRef: NotificationInstance | null = null;
+
+export const createNotification = {
+  success: (args: Omit<NotificationArgsProps, "type">) => {
+    notificationRef?.success({
+      ...args,
+      showProgress: true,
+      pauseOnHover: true,
+    });
+  },
+  error: (args: Omit<NotificationArgsProps, "type">) => {
+    notificationRef?.error({
+      ...args,
+      showProgress: true,
+      pauseOnHover: true,
+    });
+  },
+  info: (args: Omit<NotificationArgsProps, "type">) => {
+    notificationRef?.info({
+      ...args,
+      showProgress: true,
+      pauseOnHover: true,
+    });
+  },
+  warning: (args: Omit<NotificationArgsProps, "type">) => {
+    notificationRef?.warning({
+      ...args,
+      showProgress: true,
+      pauseOnHover: true,
+    });
+  },
+};
+
+// Proveedor de contexto que inyecta el `contextHolder` de Ant Design
+export const NotificationProvider = ({ children }: { children: ReactNode }) => {
+  const [api, contextHolder] = notification.useNotification();
+
+  // Guardar la instancia de notificaciones para uso global
+  useEffect(() => {
+    notificationRef = api;
+  }, [api]);
+
+  return (
+    <>
+      {contextHolder}
+      {children}
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NotificationProvider } from "./componets/NotificationCustom.tsx";
+import { ConfigProvider } from "antd";
+import esES from "antd/locale/es_ES";
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+dayjs.locale("es");
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <NotificationProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider locale={esES}>
+          <App />
+        </ConfigProvider>
+      </QueryClientProvider>
+    </NotificationProvider>
   </StrictMode>,
-)
+);
